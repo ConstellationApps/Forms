7e9ab31111cc1d87b95bf25bd6ff8542c4c273f0
Michael Aldridge <aldridge.mac@gmail.com>
2017-02-28 21:12:52 -0600

    Enable Tests
    This commit enables tests with Django as well as automatic linting
    with flake8.  As part of the changes for this commit the project has
    also been altered to follow the standard Python Egg source layout.

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='Constellation-Forms',
+    version='0.1.0',
+    packages=['constellation_forms'],
+    include_package_data=True,
+    license='ISC License',
+    description='Constellation Suite - Forms',
+    long_description=README,
+    url='https://github.com/ConstellationApps/',
+    author='Constellation Developers',
+    author_email='bugs@constellationapps.org',
+    install_requires=[
+        'constellation_base>=0.1.3',
+        'django-guardian',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Development Status :: 4 - Beta'
+    ]
+)
---
+++ testDjango/settings.py
@@ -0,0 +1,86 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'uh$e51f+k)a%047(&o0v__f2$zx2otmil5w1$)k6u9e_&ue75l'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+LOGIN_URL = '/login'
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'guardian',
+    'constellation_base',
+    'constellation_forms',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',  # this is default
+    'guardian.backends.ObjectPermissionBackend',
+)
+
+ROOT_URLCONF = 'testDjango.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Password validation (None since this is a test install)
+AUTH_PASSWORD_VALIDATORS = []
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Media files (Components that are user submitted)
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
