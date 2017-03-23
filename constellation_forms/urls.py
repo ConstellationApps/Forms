abf80aecc39bd6fd3a783dd6734ea9b611fde302
dinoocch <dino.occhialini@gmail.com>
2017-03-22 22:51:36 -0500

    Add form submission backend
    This commit adds the ability to submit a form via the web interace and
    parse the resulting json data into a model.  In addition, we rely on the
    model validation to check the forms correctness.

--- constellation_forms/urls.py
+++ constellation_forms/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     # Management Functions
-    url(r'^manage/create-form$', views.manage_create_form,
+    url(r'^manage/create-form$', views.manage_create_form.as_view(),
         name="manage_create_form"),
 ]
--- constellation_forms/views.py
+++ constellation_forms/views.py
@@ -1,16 +1,70 @@
 from django.contrib.auth.models import Group
 from django.shortcuts import render
-
+from django.views import View
 from constellation_base.models import GlobalTemplateSettings
+from .models import Form
+
+import json
 
 
-def manage_create_form(request):
-    ''' Returns a page that allows for the creation of new forms '''
-    template_settings_object = GlobalTemplateSettings(allowBackground=False)
-    template_settings = template_settings_object.settings_dict()
-    groups = [(g.name, g.pk) for g in Group.objects.all()]
+class manage_create_form(View):
+    def get(self, request):
+        ''' Returns a page that allows for the creation of new forms '''
+        template_settings = GlobalTemplateSettings(allowBackground=False)
+        template_settings = template_settings.settings_dict()
+        groups = [(g.name, g.pk) for g in Group.objects.all()]
 
-    return render(request, 'constellation_forms/create-form.html', {
-        'groups': groups,
-        'template_settings': template_settings,
-    })
+        return render(request, 'constellation_forms/create-form.html', {
+            'groups': groups,
+            'template_settings': template_settings,
+        })
+
+    def post(self, request):
+        ''' Creates a form '''
+        form_data = json.dumps(request.POST['data'])
+        title = form_data['meta']['title']
+        description = form_data['meta']['description']
+        widgets = []
+        for widget in form_data['widgets']:
+            temp_widget = {}
+            temp_widget['type'] = widget['type']
+            if 'title' in widget:
+                temp_widget['title'] = widget['title']
+            if 'description' in widget:
+                temp_widget['description'] = widget['description']
+            if 'required' in widget and widget['required'] == 'on':
+                temp_widget['required'] = True
+            else:
+                temp_widget['required'] = False
+            if 'validator' in widget:
+                temp_widget['validator'] = widget['validator']
+            choices = [(int(k.split('-')[1]), v) for k, v in widget.items()
+                       if 'choice' in k]
+            if len(choices) > 0:
+                if 'other-allowed' in widget and 'other-allowed' == 'on':
+                    temp_widget['other_allowed'] = True
+                else:
+                    temp_widget['other_allowed'] = False
+                choices.sort()
+                temp_widget['choices'] = [v[1] for v in choices]
+            widgets.append(temp_widget)
+
+        # This is not safe, but it will work for now...
+        if Form.objects.all().count() > 0:
+            form_id = Form.objects.all().order_by("-form_id")[0] + 1
+        else:
+            form_id = 1
+
+        new_form = Form(
+            version=1,
+            form_id=form_id,
+            name=title,
+            description=description,
+            elements=widgets,
+        )
+        try:
+            new_form.full_clean()
+            new_form.save()
+        except:
+            # invalid form
+            pass
