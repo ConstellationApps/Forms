43567fe607a57bf4b4030301119af8dc4e47438c
Matthew Wynn <matthew@matthewwynn.com>
2017-03-30 15:30:53 -0500

    Signature widget and required validation

--- constellation_forms/static/constellation_forms/js/submit-form.js
+++ constellation_forms/static/constellation_forms/js/submit-form.js
@@ -1,18 +1,50 @@
-/* exported addWidget deleteWidget addChoice deleteChoice submitForm */
+/* exported submitForm */
+
+$('.mdl-textfield__input').one('blur keydown', function() {
+  $(this).parent().addClass('touched');
+});
+
+/**
+ * Turns on invalid CSS for every textfield,
+ * regardless of whether it's been entered yet
+ */
+function turnOnValidCSS() {
+  $('.mdl-textfield__input').each(function() {
+    $(this).parent().addClass('touched');
+  });
+}
+
+/**
+ * Validate a widget
+ */
+function validate(elem) {
+  return elem.get()[0].checkValidity();
+}
 
 /**
  * Collect the widgets and submit them to the backend
  */
 function submitForm() {
+  turnOnValidCSS();
   let widgetForm = {'widgets': []};
   let i = 0;
   let csrfToken = $.cookie('csrftoken');
+  let valid = true;
   $('#widgets-holder').find('form').each(function() {
+    if (valid && !validate($(this))) {
+      $('.mdl-layout').animate({
+        scrollTop: $(this).offset().top - $('.mdl-layout__content').offset().top,
+      }, 200);
+      valid = false;
+    }
     let widget = $(this).serializeArray();
-    if (widget.length() > 0) {
+    if (widget.length > 0) {
       widgetForm.widgets[i++] = $(this).serializeArray()[0]['value'];
     }
   });
+  if (!valid) {
+    return false;
+  }
   let data = {
     'csrfmiddlewaretoken': csrfToken,
     'data': JSON.stringify(widgetForm),
